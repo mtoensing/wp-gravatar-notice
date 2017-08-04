441c8fd9af03308adbd71fcccc659bcc1a869144
Marc Tönsing <marc.toensing@gmail.com>
2017-08-04 14:08:29 +0200

    First commit

---
+++ readme.txt
@@ -0,0 +1,19 @@
+=== WP Gravatar Notice ===
+Contributors: MarcDK
+Tags: marctv, gravatar, legal, notice
+Requires at least: 3.0
+Tested up to: 4.8
+Stable tag: 1.0
+
+== Description ==
+
+Displays a notice at the comment section that the blog uses the gravatars service for commenter's user image.
+
+== Installation ==
+
+* Install plugin
+* Activate it
+
+== Changelog ==
+
+1.0 Initial version
---
+++ wp-gravatar-notice.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+Plugin Name: WP Gravatar Notice
+Description: Displays a notice that this blog uses the gravatars service for user images in the comment section.
+Plugin URI:  http://marc.tv
+Version:     0.1
+Text Domain: wp-gravatar-notice
+Domain Path: /language
+Author:      MarcDK
+Author URI:  http://marc.tv
+License URI: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+*/
+
+add_filter( 'comment_form_defaults', 'wpgn_comment_form_defaults' );
+
+function wpgn_comment_form_defaults( $defaults ) {
+
+	$text                            = __( 'This website displays user images from %s.', 'wp-gravatar-notice' );
+	$defaults['comment_notes_after'] = '<p class="comment-notes">' . sprintf( $text, '<a rel="nofollow" href="https://gravatar.com">gravatar.com</a>' ) . '</p>';
+
+	return $defaults;
+}
+?>
